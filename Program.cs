e86f8c334595e5987f8cbcb6ad0e325a5f50a79d
RezaRauf <89615965+RezaRauf@users.noreply.github.com>
2021-08-27 04:09:00 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+namespace MyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int TestScore1, TestScore2, TestScore3, TestScore4, TestScore5;
+            double average;
+
+            WriteLine("Please enter your score for TEST 1");
+            TestScore1 = Convert.ToInt32(ReadLine());
+
+            WriteLine("Please enter your score for TEST 2");
+            TestScore2 = Convert.ToInt32(ReadLine());
+
+            WriteLine("Please enter your score for TEST 3");
+            TestScore3 = Convert.ToInt32(ReadLine());
+
+            WriteLine("Please enter your score for TEST 4");
+            TestScore4 = Convert.ToInt32(ReadLine());
+
+            WriteLine("Please enter you sccore for TEST 5");
+            TestScore5 = Convert.ToInt32(ReadLine());
+
+            average = Convert.ToDouble (TestScore1 + TestScore2 + TestScore3 + TestScore4 + TestScore5) / 5;
+            
+          
+            WriteLine("YOUR AVERAGE SCORE IS {0}", average.ToString("F2"));
+        } 
+    }
+}
